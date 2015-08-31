ce461215623265ef490330dc6d2cb4c7b9613d45
Dnld <steinert.donald@gmail.com>
2015-08-31 18:36:31 -0400

    retreives messages from server with ajax call

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,2 +1,17 @@
-// YOUR CODE HERE:
 
+// ajax get all check on messages
+var getMessages = function() {
+  $.ajax({
+    url: 'https://api.parse.com/1/classes/chatterbox',
+    type: 'GET',
+    success: function(data) {
+      console.log('success');
+      console.log(data);
+    },
+    error: function() {
+      console.log('error');
+    },
+  });
+};
+
+getMessages();
