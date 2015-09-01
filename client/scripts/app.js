fd098198b3e6f9df2d0cd4e76e4add1244b2a6d4
Dnld <steinert.donald@gmail.com>
2015-08-31 20:11:25 -0400

    implements functionality to display messages, adds initial styles

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,16 +1,31 @@
+var $chatFeed = $('.chat');
+
+var displayMessages = function(data) {
+  console.log(data);
+  var messages = data.results;
+  for (var i = 0; i < messages.length; i++) {
+    var message = messages[i];
+    var $date = ($.format.date(message.createdAt, 'MMM d h:mm p'));
+    var $newMessage = $('<div class="message"></div>');
+    $newMessage.html(
+      '<div class="message-date">' + $date +
+      '</div><div class="username">' + _.escape(message.username) +
+      '</div><div class="message-text">' + _.escape(message.text) + '</div>');
+    $newMessage.prependTo($chatFeed);
+  }
+};
+
+var errorHandler = function() {
+  console.error('error');
+};
 
 // ajax get all check on messages
 var getMessages = function() {
   $.ajax({
     url: 'https://api.parse.com/1/classes/chatterbox',
     type: 'GET',
-    success: function(data) {
-      console.log('success');
-      console.log(data);
-    },
-    error: function() {
-      console.log('error');
-    },
+    success: displayMessages,
+    error: errorHandler
   });
 };
 
