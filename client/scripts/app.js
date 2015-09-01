158c7d86cc01cb48f5d3abf6cf11ddbf9a68128a
Dnld <steinert.donald@gmail.com>
2015-08-31 22:16:41 -0400

    implements continuous update

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,25 +1,41 @@
 var $chatFeed = $('.chat');
+var recentMessageId;
 
 var displayMessages = function(data) {
   console.log(data);
   var messages = data.results;
-  for (var i = 0; i < messages.length; i++) {
-    var message = messages[i];
-    var $date = ($.format.date(message.createdAt, 'MMM d h:mm p'));
+  var message;
+  var i;
+
+  // Eliminate previously displayed messages
+  if (recentMessageId !== undefined) {
+    for (i = 0; i < messages.length; i++) {
+      if (messages[i].objectId === recentMessageId) {
+        messages.splice(i, messages.length);
+      }
+    }
+  }
+
+  // add messages to document in reverse chronological order
+  for (i = messages.length-1; i >= 0; i--) {
+    message = messages[i];
+    var $date = ($.format.date(message.createdAt, 'MMM d h:mm:ss p'));
     var $newMessage = $('<div class="message"></div>');
     $newMessage.html(
       '<div class="message-date">' + $date +
       '</div><div class="username">' + _.escape(message.username) +
       '</div><div class="message-text">' + _.escape(message.text) + '</div>');
     $newMessage.prependTo($chatFeed);
+    recentMessageId = message.objectId;
   }
 };
+
 
 var errorHandler = function() {
   console.error('error');
 };
 
-// ajax get all check on messages
+// ajax fetch chat messages
 var getMessages = function() {
   $.ajax({
     url: 'https://api.parse.com/1/classes/chatterbox',
@@ -29,4 +45,6 @@ var getMessages = function() {
   });
 };
 
+// Initialize feed and fetch messages every 1s
 getMessages();
+setInterval(getMessages, 1000);
